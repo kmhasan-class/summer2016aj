96badf41fea45401a530fb7265eca990265710af
Monirul Hasan <kmhasan@gmail.com>
2016-08-16 23:49:23 +0600

    simple branching demo

--- DAO pattern/src/dao/pattern/DAOPattern.java
+++ DAO pattern/src/dao/pattern/DAOPattern.java
@@ -24,6 +24,7 @@ public class DAOPattern extends Application {
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
+        stage.setFullScreen(true);
         stage.show();
     }
 
