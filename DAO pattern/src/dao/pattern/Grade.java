9fe7e4b92a1ce4e8b91b6550d07e5594a09563f8
student <student@SEUPC0284>
2016-08-17 10:59:02 +0600

    Add enum class

---
+++ DAO pattern/src/dao/pattern/Grade.java
@@ -0,0 +1,25 @@
+package dao.pattern;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author student
+ */
+public enum Grade {
+    A("A",3.75),
+    A_MINUS("A-",3.50),
+    A_PLUS("A+",4.00);
+    private String letterGrade;
+    private double numericGrade;
+
+    private Grade(String letterGrade, double numericGrade) {
+        this.letterGrade = letterGrade;
+        this.numericGrade = numericGrade;
+    }
+    
+}
